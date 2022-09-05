f54723020cf2386a065a5c3093aec73a3690028d
Marcos Salas <keickd16@gmail.com>
2022-09-05 23:59:41 +0200

    page view

---
+++ lib/pages/tabs_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  const _Navegacion({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), label: 'Para ti'),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados')
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const BouncingScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
+    );
+  }
+}
